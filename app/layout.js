9878056545d05f9fcbe6b6f2aef62f11e164f3cd
dermiie <demioyeniyi@gmail.com>
2026-04-29 09:47:02 +0100

    feat- pages updated & supabse installed

--- app/layout.js
+++ app/layout.js
@@ -21,8 +21,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="mx-auto max-w-7xl ">{children}</main>
+        <div className="grid flex-1 px-8 py-12">
+          <main className="w-full mx-auto max-w-7xl">{children}</main>
         </div>
       </body>
     </html>
