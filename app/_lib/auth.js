4acf3008c3bca65569b66f429cf7019b6a56208a
dermiie <demioyeniyi@gmail.com>
2026-07-20 07:38:21 +0100

    feat- attach newly created guestId to our auth session

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { createGuest, getGuest } from './data-service';
 
 const authConfig = {
   providers: [
@@ -12,8 +13,29 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       // return !!auth?.user; //short-hand for the below
-
+      // console.log(request);
       return auth?.user ? true : false;
+    },
+
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+
+    async session({ session, user }) {
+      console.log(session);
+      const guest = await getGuest(session.user.email);
+
+      session.user.id = guest.id; //Add the newly created guestId to our session.user object
+
+      return session; //Always return session to avoid "no more session" error
     },
   },
 
